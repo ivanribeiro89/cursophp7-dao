c57dc619634129146d4120c0e043e3301b415702
Ivan Ribeiro <irr@habitasystem.com.br>
2017-07-21 16:17:25 -0300

    inserir e alterar

--- class/usuario.php
+++ class/usuario.php
@@ -38,11 +38,7 @@ class Usuario{
 			":ID"=>$id
 			));
 		if (count($result) > 0){
-			$row = $result[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($result[0]);
 		}
 	}
 
@@ -66,15 +62,45 @@ class Usuario{
 			":PASSWORD"=>$password
 			));
 		if (count($result) > 0){
-			$row = $result[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($result[0]);
 		} else {
 			throw new Exception("Login e/ou senha inválidos.");			
 		}
 	}
+	public function setData($data){
+			$this->setIdusuario($data['idusuario']);
+			$this->setDeslogin($data['deslogin']);
+			$this->setDessenha($data['dessenha']);
+			$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert(){
+		$sql = new Sql();
+		$result = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+		if (count($result) > 0){
+			$this->setData($result[0]);
+		}
+	}
+
+	public function update($login, $password){
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+		$sql = new Sql();
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+	}
+
+	public function __construct($login = "", $password = ""){
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+	}
+
 	public function __toString(){
 		return json_encode(array(
 			"idusuario"=>$this->getIdusuario(),
--- index.php
+++ index.php
@@ -11,27 +11,44 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 
 */
-// carrega apenas 1 usuario
-//$root = new Usuario();
-//$root->loadbyId(12);
-//echo $root;
- 
-// carrega uma lista de usuario
-//$lista = Usuario::getList();
-
-//echo json_encode($lista);
-
-// carrega uma lista de usuario buscando pelo login
-
-//$search = Usuario::search("oo");
-
-//echo json_encode($search);
-
+/*
+carrega apenas 1 usuario
+$root = new Usuario();
+$root->loadbyId(12);
+echo $root;
+*/
+/*
+carrega uma lista de usuario
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+/*
+carrega uma lista de usuario buscando pelo login
+$search = Usuario::search("oo");
+echo json_encode($search);
+*/
+/*
 //carrega um ususario usando o login e a senha
 $usuario = new Usuario();
 $usuario->login("root","1234");
 
 echo $usuario;
+*/
+/*
+criando novo usuario
+$aluno = new Usuario("aluno2017", "888555");
 
+$aluno->insert();
+
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadbyId(13);
+
+$usuario->update("ivan ribeiro", "011189");
+
+echo $usuario;
 
  ?>
