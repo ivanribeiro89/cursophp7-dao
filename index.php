24c4b8c027e10817b000bb68d8ab707f4c274ee1
Ivan Ribeiro <irr@habitasystem.com.br>
2017-07-21 15:51:46 -0300

    inclusao de listas no ususarios

--- index.php
+++ index.php
@@ -11,11 +11,27 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 
 */
+// carrega apenas 1 usuario
+//$root = new Usuario();
+//$root->loadbyId(12);
+//echo $root;
+ 
+// carrega uma lista de usuario
+//$lista = Usuario::getList();
 
-$root = new Usuario();
+//echo json_encode($lista);
 
-$root->loadbyId(12);
+// carrega uma lista de usuario buscando pelo login
 
-echo $root;
+//$search = Usuario::search("oo");
+
+//echo json_encode($search);
+
+//carrega um ususario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","1234");
+
+echo $usuario;
+
 
  ?>
